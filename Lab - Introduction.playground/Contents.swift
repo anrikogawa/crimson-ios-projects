7eb01927a98e4fb44e82447ba05d39ab53fa6fa4
anrikogawa <102395973+anrikogawa@users.noreply.github.com>
2022-03-26 10:32:39 +0900

    Update Contents.swift

--- Lab - Introduction.playground/Contents.swift
+++ Lab - Introduction.playground/Contents.swift
@@ -8,15 +8,17 @@ print("I have learned the following:")
 print("What features make Swift a modern and safe language")
 print("How to use the Swift REPL in Terminal")
 print("How to use playgrounds to make writing Swift fun and simple")
+print("How to start edit with github")
  
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
+print("Tile: You bring no sadness"\"Artist: Sondia")
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
+print("You're not sadness in my heart")
 
 
 //:page 14 of 16  |  [Next: Exercise: Go! Fight! Win!](@next)
